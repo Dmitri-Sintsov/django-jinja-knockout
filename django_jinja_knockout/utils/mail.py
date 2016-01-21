e8e1ff87f418c652aaf5179076c5c44ebb45523e
Dmitriy Sintsov <questpc256@gmail.com>
2016-01-21 17:00:15 +0300

    utils.mail.send_mail() now has optional built-in error handler which uses django message framework to display sendmail errors.

--- django_jinja_knockout/utils/mail.py
+++ django_jinja_knockout/utils/mail.py
@@ -1,7 +1,10 @@
+from smtplib import SMTPDataError
 from bleach import linkify
 from django.conf import settings
 from django.utils.html import linebreaks
+from django.utils.translation import ugettext_lazy as _
 from django.core.mail import send_mail as django_send_mail
+from django.contrib import messages
 
 
 def send_mail(**kwargs):
@@ -13,4 +16,17 @@ def send_mail(**kwargs):
     if 'fail_silently' not in kwargs:
         kwargs['fail_silently'] = False
 
-    return django_send_mail(**kwargs)
+    if 'request' not in kwargs:
+        return django_send_mail(**kwargs)
+
+    request = kwargs.pop('request')
+    try:
+        # raise SMTPDataError(code=123, msg='Big error')
+        return django_send_mail(**kwargs)
+    except SMTPDataError as e:
+        msg = _('Error "%(code)s" "%(msg)s" while sending email to %(email_list)s.') % {
+            'code': e.smtp_code,
+            'msg': e.smtp_error,
+            'email_list': ', '.join(['"{}"'.format(email) for email in kwargs['recipient_list']])
+        }
+        messages.error(request, msg)
