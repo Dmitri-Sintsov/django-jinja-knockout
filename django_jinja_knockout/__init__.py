4ae96db74759e01fee6793024794cb3497b556a8
Dmitriy Sintsov <questpc256@gmail.com>
2016-08-13 14:55:36 +0300

    Package installation fixes.

--- django_jinja_knockout/__init__.py
+++ django_jinja_knockout/__init__.py
@@ -4,11 +4,7 @@ __version__ = '0.2.0'
 # Required by Bootstrap datetimepicker to work with 'en' locales.
 def set_datetime_12_hour_format(lang_code, use_l10n):
     from django.utils.formats import get_format, _format_cache
-    try:
         get_format('DATETIME_INPUT_FORMATS', lang_code, use_l10n)
-    except ImproperlyConfigured:
-        # Not an error, but a pypi package installation mode.
-        return
     for key in _format_cache:
         if key[0] == 'DATETIME_INPUT_FORMATS':
             _format_cache[key] += ('%m/%d/%Y %I:%M %p',)
@@ -17,7 +13,12 @@ try:
     from django.core.exceptions import ImproperlyConfigured
     from django.conf import settings
 
-    set_datetime_12_hour_format(settings.LANGUAGE_CODE, settings.USE_L10N)
+    try:
+        set_datetime_12_hour_format(settings.LANGUAGE_CODE, settings.USE_L10N)
+    except ImproperlyConfigured:
+        # Django is not configured yet.
+        pass
 
 except ImportError:
+    # Django is not installed yet.
     pass
