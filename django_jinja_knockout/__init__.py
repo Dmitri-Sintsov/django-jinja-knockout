cb9b11055cfda2039493328ce1b4e7c7cf018b71
Dmitriy Sintsov <questpc256@gmail.com>
2016-08-13 14:58:26 +0300

    Package installation fixes.

--- django_jinja_knockout/__init__.py
+++ django_jinja_knockout/__init__.py
@@ -4,7 +4,8 @@ __version__ = '0.2.0'
 # Required by Bootstrap datetimepicker to work with 'en' locales.
 def set_datetime_12_hour_format(lang_code, use_l10n):
     from django.utils.formats import get_format, _format_cache
-        get_format('DATETIME_INPUT_FORMATS', lang_code, use_l10n)
+
+    get_format('DATETIME_INPUT_FORMATS', lang_code, use_l10n)
     for key in _format_cache:
         if key[0] == 'DATETIME_INPUT_FORMATS':
             _format_cache[key] += ('%m/%d/%Y %I:%M %p',)
