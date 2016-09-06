2355e328068383b276f1873c7ab23740339db841
Dmitriy Sintsov <questpc256@gmail.com>
2016-09-06 11:55:56 +0300

    Support of 'with' and 'only' arguments in jinja DTL template tag.

--- django_jinja_knockout/templatetags/jinja.py
+++ django_jinja_knockout/templatetags/jinja.py
@@ -1,42 +1,94 @@
+from django.utils import six
 from django import template
 from django.template.loader import _engine_list
+from django.template.base import token_kwargs
 from django_jinja import backend
 
 register = template.Library()
 
 
-def do_jinja(parser, token):
-    try:
-        # split_contents() knows not to split quoted strings.
-        tag_name, format_string = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError(
-            "%r tag requires a single argument" % token.contents.split()[0]
-        )
-    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
-        raise template.TemplateSyntaxError(
-            "%r tag's argument should be in quotes" % tag_name
-        )
-    return JinjaNode(format_string[1:-1])
-
-
 class JinjaNode(template.Node):
 
-    def __init__(self, template_name):
-        self.template_name = template_name
+    def __init__(self, tpl, *args, **kwargs):
+        self.template = tpl
+        self.extra_context = kwargs.pop('extra_context', {})
+        self.isolated_context = kwargs.pop('isolated_context', False)
+        super(JinjaNode, self).__init__(*args, **kwargs)
 
-    def render(self, context):
+    def get_jinja_engine(self):
         engines = _engine_list()
         for engine in engines:
             if isinstance(engine, backend.Jinja2):
-                t = engine.get_template(self.template_name)
-                template_context = {}
-                # Merge context dicts from all context processors available.
-                for context_dict in context:
-                    template_context.update(context_dict)
-                return t.render(request=context.request, context=context_dict)
+                return engine
         raise ValueError('Cannot find django_jinja.backend.Jinja2')
 
+    def render(self, context):
+        try:
+            tpl = self.template.resolve(context)
+            # Does this quack like a Template?
+            if not callable(getattr(tpl, 'render', None)):
+                # If not, we'll try get_template
+                tpl = self.get_jinja_engine().get_template(tpl)
+            isolated_values = {
+                name: var.resolve(context)
+                for name, var in six.iteritems(self.extra_context)
+            }
+            if self.isolated_context:
+                return tpl.render(request=context.request, context=isolated_values)
+            context_values = {}
+            # Merge context dicts from all context processors available.
+            for context_dict in context:
+                context_values.update(context_dict)
+            context_values.update(isolated_values)
+            return tpl.render(request=context.request, context=context_values)
+        except Exception:
+            if context.template.engine.debug:
+                raise
+            return ''
 
-# {% jinja 'template.htm' %}
-register.tag('jinja', do_jinja)
+
+@register.tag('jinja')
+def do_jinja(parser, token):
+    """
+    Loads a Jinja2 template and renders it with the current context. You can pass
+    additional context using keyword arguments.
+
+    Example::
+
+        {% jinja "foo/some_include" %}
+        {% jinja "foo/some_include" with bar="BAZZ!" baz="BING!" %}
+
+    Use the ``only`` argument to exclude the current context when rendering
+    the included template::
+
+        {% jinja "foo/some_include" only %}
+        {% jinja "foo/some_include" with bar="1" only %}
+    """
+    tokens = token.split_contents()
+    if len(tokens) < 2:
+        raise template.TemplateSyntaxError(
+            "%r tag takes at least one argument: the name of the template to "
+            "be included." % tokens[0]
+        )
+    options = {}
+    remaining_tokens = tokens[2:]
+    while remaining_tokens:
+        option = remaining_tokens.pop(0)
+        if option in options:
+            raise template.TemplateSyntaxError('The %r option was specified more '
+                    'than once.' % option)
+        if option == 'with':
+            value = token_kwargs(remaining_tokens, parser, support_legacy=False)
+            if not value:
+                raise template.TemplateSyntaxError('"with" in %r tag needs at least '
+                        'one keyword argument.' % tokens[0])
+        elif option == 'only':
+            value = True
+        else:
+            raise template.TemplateSyntaxError('Unknown argument for %r tag: %r.' %
+                    (tokens[0], option))
+        options[option] = value
+    isolated_context = options.get('only', False)
+    namemap = options.get('with', {})
+    return JinjaNode(parser.compile_filter(tokens[1]), extra_context=namemap,
+                       isolated_context=isolated_context)
