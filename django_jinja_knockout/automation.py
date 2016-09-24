43e1700cbfc37c2c4b8ad2fa8caae4ad65b9246f
Dmitriy Sintsov <questpc256@gmail.com>
2016-09-24 22:05:44 +0300

    Fixed automation.AutomationCommands usage as mixin.

--- django_jinja_knockout/automation.py
+++ django_jinja_knockout/automation.py
@@ -1,6 +1,7 @@
 class AutomationCommands:
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.last_result = None
 
     def exec(self, *args):
