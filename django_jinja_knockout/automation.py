137ed8e0ede51b7a79c9eea9e616b315f6fb1e76
Dmitriy Sintsov <questpc256@gmail.com>
2016-09-24 21:45:04 +0300

    Added automation.AutomationCommands and testing.SeleniumCommands classes to simplify Selenium testing.

---
+++ django_jinja_knockout/automation.py
@@ -0,0 +1,26 @@
+class AutomationCommands:
+
+    def __init__(self):
+        self.last_result = None
+
+    def exec(self, *args):
+        self.last_result = None
+        method = None
+        last_opcode = None
+        for key, opcode in enumerate(args):
+            if isinstance(opcode, str):
+                method = getattr(self, 'do_{}'.format(opcode))
+                if key == len(args) - 1 or isinstance(args[key + 1], str):
+                    self.last_result = method()
+            elif isinstance(opcode, (tuple, list)):
+                if key == len(args) - 1 or not isinstance(args[key + 1], dict):
+                    self.last_result = method(*opcode)
+            elif isinstance(opcode, dict):
+                if isinstance(last_opcode, (tuple, list)):
+                    self.last_result = method(*last_opcode, **opcode)
+                else:
+                    self.last_result = method(**opcode)
+            else:
+                raise ValueError(
+                    'Invalid opcode, should be str (method name), tuple (method args) or dict (method kwagrs)')
+            last_opcode = opcode
---
+++ django_jinja_knockout/testing.py
@@ -0,0 +1,53 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from .automation import AutomationCommands
+from .tpl import reverseq
+
+
+"""
+Selenium tests may require Firefox ESR because Ubuntu sometimes updates Firefox to newer version
+than currently installed Selenium supports.
+
+Here is the example of installing Firefox ESR in Ubuntu 14.04:
+
+apt-get remove firefox
+wget http://ftp.mozilla.org/pub/firefox/releases/45.4.0esr/linux-x86_64/en-US/firefox-45.4.0esr.tar.bz2
+tar -xvjf firefox-45.4.0esr.tar.bz2 -C /opt
+ln -s /opt/firefox/firefox /usr/bin/firefox
+
+Do not forget to update to latest ESR when running the tests.
+"""
+
+
+class SeleniumCommands(AutomationCommands, StaticLiveServerTestCase):
+
+    WAIT_SECONDS = 10
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = cls.selenium_factory()
+        cls.selenium.implicitly_wait(cls.WAIT_SECONDS)
+
+    @classmethod
+    def tearDownClass(cls):
+        # cls.selenium.quit()
+        super().tearDownClass()
+
+    def do_reverse_url(self, viewname, kwargs=None, query=None):
+        url = '{}{}'.format(
+            self.live_server_url, reverseq(viewname=viewname, kwargs=kwargs, query=query)
+        )
+        print('get_reverse_url: {}'.format(url))
+        return self.selenium.get(url)
+
+    def do_keys_by_id(self, id, keys):
+        input = self.selenium.find_element_by_id(id)
+        input.send_keys(keys)
+        return input
+
+    def do_by_xpath(self, xpath):
+        return self.selenium.find_element_by_xpath(xpath)
+
+    def do_click(self):
+        return self.last_result.click()
