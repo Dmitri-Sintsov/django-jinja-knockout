902c6b9edf01264014bb5f6614ae7401277647dc
Dmitriy Sintsov <questpc256@gmail.com>
2016-11-08 11:54:58 +0300

    Easier to use version of tpl.format_local_date().

--- django_jinja_knockout/tpl.py
+++ django_jinja_knockout/tpl.py
@@ -143,15 +143,13 @@ def html_to_text(html):
     return doc.text_content()
 
 
-def format_local_date(value, format=None, use_l10n=None):
+def format_local_date(value, short_format=True, use_l10n=None):
     if isinstance(value, datetime):
         combined = value
-        if format is None:
-            format = 'SHORT_DATETIME_FORMAT'
+        format = 'SHORT_DATETIME_FORMAT' if short_format else 'DATETIME_FORMAT'
     elif isinstance(value, date):
         combined = datetime.combine(value, datetime.min.time())
-        if format is None:
-            format = 'SHORT_DATE_FORMAT'
+        format = 'SHORT_DATE_FORMAT' if short_format else 'DATE_FORMAT'
     else:
         raise ValueError('Value must be instance of date or datetime')
     return formats.date_format(combined, format, use_l10n)
