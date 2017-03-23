8bfd921af31f687859740ec9454c658bb29a0112
Dmitriy Sintsov <questpc256@gmail.com>
2017-03-23 13:26:03 +0300

    Fixed disambiguation of dialogOptions.message type jQuery object from simple object in App.Dialog.create().

--- django_jinja_knockout/static/js/front/app.js
+++ django_jinja_knockout/static/js/front/app.js
@@ -256,12 +256,13 @@ App.Dialog = function(options) {
         // Do not forget to escape from XSS.
         if (typeof this.dialogOptions.message === 'undefined') {
             this.dialogOptions.message = this.createDialogContent();
-        } else if (typeof this.dialogOptions.message === 'object') {
-            var $content = $('<div>');
-            this.dialogOptions.message = App.renderNestedList(
-                $content, this.dialogOptions.message, {blockTags: App.blockTags.badges}
-            );
         } else if (!(this.dialogOptions.message instanceof jQuery)) {
+            if (typeof this.dialogOptions.message === 'object') {
+                var $content = $('<div>');
+                this.dialogOptions.message = App.renderNestedList(
+                    $content, this.dialogOptions.message, {blockTags: App.blockTags.badges}
+                );
+            }
             this.dialogOptions.message = $.contents(this.dialogOptions.message);
         }
         if (typeof this.dialogOptions.method !== 'undefined') {
