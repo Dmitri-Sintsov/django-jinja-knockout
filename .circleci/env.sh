9cc717e557b7ef2e5d673340b525422ac3c9ed64
Dmitriy Sintsov <questpc256@gmail.com>
2018-12-27 11:39:24 +0300

    Circle CI / Travis CI configurations to run Bootstrap 3 / Bootstrap 4 tests.

---
+++ .circleci/env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Install pyenv to use exact version of Python.
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+pyenv init -
+pyenv install 3.6.8
+echo Version of Python installed in the python:latest-browsers image:
+python3 --version
+export PATH="$(pyenv root)/versions/3.6.8/bin/:$PATH"
+echo Version of Python used to run the tests:
+python3 --version
+# https://stackoverflow.com/questions/27849412/permissionerror-with-pip3
+python3 -m pip install -U pip wheel tox tox-pyenv setuptools --user
+git clone https://github.com/Dmitri-Sintsov/djk-sample.git
+
