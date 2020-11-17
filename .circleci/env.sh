2413015e3b8b1559dbddd98322b88d89f7398d6b
Dmitri Sintsov <questpc256@gmail.com>
2020-11-17 13:39:12 +0300

    Changed Python version test matrix.

--- .circleci/env.sh
+++ .circleci/env.sh
@@ -4,10 +4,10 @@ git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 pyenv init -
-pyenv install 3.7.9
+pyenv install 3.6.12
 echo Version of Python installed in the python:latest-browsers image:
 python3 --version
-export PATH="$(pyenv root)/versions/3.7.9/bin/:$PATH"
+export PATH="$(pyenv root)/versions/3.6.12/bin/:$PATH"
 echo Version of Python used to run the tests:
 python3 --version
 # https://stackoverflow.com/questions/27849412/permissionerror-with-pip3
