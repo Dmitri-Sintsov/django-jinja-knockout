ef98ee8f7ad115b4a597a035d34e34a189cac532
Dmitri Sintsov <questpc256@gmail.com>
2020-11-19 22:30:15 +0300

    Updated circle ci test matrix. Preliminary support of selenium 4.0a in Chromium webdriver.

--- django_jinja_knockout/webdriver/chromium/webdriver.py
+++ django_jinja_knockout/webdriver/chromium/webdriver.py
@@ -1,5 +1,7 @@
+import inspect
 from selenium.webdriver.chrome.webdriver import WebDriver as ChromeWebDriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 
 class WebDriver(ChromeWebDriver):
@@ -13,15 +15,19 @@ class WebDriver(ChromeWebDriver):
     def __init__(self, *args, **kwargs):
 
         chrome_options = Options()
-
         if self.is_headless:
             chrome_options.add_argument('--headless')
         if self.window_size:
             chrome_options.add_argument('--window-size={},{}'.format(*self.window_size))
         if self.chrome_path:
             chrome_options.binary_location = self.chrome_path
-        if self.webdriver_path:
-            kwargs['executable_path'] = self.webdriver_path
-
         kwargs['options'] = chrome_options
+        if self.webdriver_path:
+            webdriver_args = inspect.signature(ChromeWebDriver.__init__)
+            print(webdriver_args)
+            if 'service' in webdriver_args.parameters:
+                # Selenium>=4.0
+                kwargs['service'] = Service(self.webdriver_path)
+            else:
+                kwargs['executable_path'] = self.webdriver_path
         super().__init__(*args, **kwargs)
