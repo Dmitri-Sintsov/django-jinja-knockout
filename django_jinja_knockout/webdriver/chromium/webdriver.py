050ec1c0bd8072d4f71ebd4ce0ce0d92e9e26ed1
Dmitriy Sintsov <questpc256@gmail.com>
2021-02-17 23:08:47 +0300

    Cleaned up Selenium driver code. Improved github CI logging.

--- django_jinja_knockout/webdriver/chromium/webdriver.py
+++ django_jinja_knockout/webdriver/chromium/webdriver.py
@@ -10,7 +10,6 @@ class WebDriver(ChromeWebDriver):
 
     is_headless = False
     enable_logging = False
-    remote_debugging_port = None
     window_size = False
     # Ubuntu snap path
     chrome_path = '/usr/bin/chromium-browser'
@@ -21,13 +20,8 @@ class WebDriver(ChromeWebDriver):
         if self.is_headless:
             # https://stackoverflow.com/questions/22424737/unknown-error-chrome-failed-to-start-exited-abnormally
             # https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('--headless')
             self.enable_logging = True
-            self.remote_debugging_port = 9222
-        if self.remote_debugging_port:
-            chrome_options.add_argument(f'--remote-debugging-port={self.remote_debugging_port}')
         if self.window_size:
             chrome_options.add_argument('--window-size={},{}'.format(*self.window_size))
         if self.chrome_path:
@@ -39,10 +33,8 @@ class WebDriver(ChromeWebDriver):
         service_kwargs = {
             'executable_path': self.webdriver_path if self.webdriver_path else 'chromedriver'
         }
-        if self.remote_debugging_port:
-            service_kwargs['port'] = self.remote_debugging_port
         if self.is_headless:
-            service_kwargs['service_args'] = ['--verbose', '--headless']
+            service_kwargs['service_args'] = ['--verbose']
         if self.log_file:
             service_kwargs['log_path'] = self.log_file.name
         return Service(**service_kwargs)
@@ -75,8 +67,8 @@ class WebDriver(ChromeWebDriver):
         except WebDriverException:
             if self.enable_logging:
                 self.log_file.seek(0)
-                print('Begin of Selenoum log file')
+                print(f'Begin of Selenium log file {self.log_file.name}')
                 print(self.log_file.read())
-                print('End of Selenium log file')
+                print(f'End of Selenium log file {self.log_file.name}')
                 self.log_file.close()
             raise
