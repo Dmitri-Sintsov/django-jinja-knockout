9fc3b36ea82deea51fd6c427f9b64cdeeac7e119
Dmitriy Sintsov <questpc256@gmail.com>
2021-02-17 21:59:01 +0300

    Updated Chromium Selenium driver logging / initialization.

--- django_jinja_knockout/webdriver/chromium/webdriver.py
+++ django_jinja_knockout/webdriver/chromium/webdriver.py
@@ -16,10 +16,8 @@ class WebDriver(ChromeWebDriver):
     chrome_path = '/usr/bin/chromium-browser'
     webdriver_path = '/snap/bin/chromium.chromedriver'
 
-    def __init__(self, *args, **kwargs):
-
+    def _get_init_options(self):
         chrome_options = Options()
-        log_file = None
         if self.is_headless:
             # https://stackoverflow.com/questions/22424737/unknown-error-chrome-failed-to-start-exited-abnormally
             # https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t
@@ -28,35 +26,57 @@ class WebDriver(ChromeWebDriver):
             chrome_options.add_argument('--headless')
             self.enable_logging = True
             self.remote_debugging_port = 9222
-        if self.enable_logging:
-            log_file = tempfile.NamedTemporaryFile(mode='r+', delete=False, encoding='utf-8')
         if self.remote_debugging_port:
             chrome_options.add_argument(f'--remote-debugging-port={self.remote_debugging_port}')
         if self.window_size:
             chrome_options.add_argument('--window-size={},{}'.format(*self.window_size))
         if self.chrome_path:
             chrome_options.binary_location = self.chrome_path
-        kwargs['options'] = chrome_options
+        return chrome_options
+
+    # Selenium >=4.0
+    def _get_service(self):
+        service_kwargs = {
+            'executable_path': self.webdriver_path if self.webdriver_path else 'chromedriver'
+        }
+        if self.remote_debugging_port:
+            service_kwargs['port'] = self.remote_debugging_port
+        if self.is_headless:
+            service_kwargs['service_args'] = ['--verbose', '--headless']
+        if self.log_file:
+            service_kwargs['log_path'] = self.log_file.name
+        return Service(**service_kwargs)
+
+    def _get_legacy_kwargs(self):
+        kwargs = {}
         if self.webdriver_path:
-            webdriver_args = inspect.signature(ChromeWebDriver.__init__)
-            if 'service' in webdriver_args.parameters:
-                # Selenium>=4.0
-                kwargs['service'] = Service(
-                    self.webdriver_path,
-                    service_args=['--verbose'],
-                    log_path=log_file.name
-                )
-            else:
-                kwargs.update({
-                    'executable_path': self.webdriver_path,
-                    'service_args': ['--verbose'],
-                    'service_log_path': self.log_path
-                })
+            kwargs['executable_path'] = self.webdriver_path
+        if self.is_headless:
+            kwargs['service_args'] = ['--verbose']
+        if self.log_file:
+            kwargs['service_log_path'] = self.log_file.name
+        return kwargs
+
+    def __init__(self, *args, **kwargs):
+        kwargs['options'] = self._get_init_options()
+        self.log_file = None
+        if self.enable_logging:
+            self.log_file = tempfile.NamedTemporaryFile(mode='r+', delete=False, encoding='utf-8')
+        webdriver_args = inspect.signature(ChromeWebDriver.__init__)
+        if 'service' in webdriver_args.parameters:
+            # Selenium>=4.0
+            kwargs['service'] = self._get_service()
+        else:
+            kwargs.update(
+                self._get_legacy_kwargs()
+            )
         try:
             super().__init__(*args, **kwargs)
         except WebDriverException:
             if self.enable_logging:
-                log_file.seek(0)
-                print(log_file.read())
-                log_file.close()
+                self.log_file.seek(0)
+                print('Begin of Selenoum log file')
+                print(self.log_file.read())
+                print('End of Selenium log file')
+                self.log_file.close()
             raise
